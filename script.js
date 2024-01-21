35522acdd9678c379e7413db7de59618f6480fb8
torrigreen <tgreen14@tulane.edu>
2024-01-21 15:19:28 -0600

    Implement user input

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const grid = document.querySelector(`.grid`);
 
-for (let i = 0; i < 256; i++) {
-	const div = document.createElement(`div`);
-	div.classList.add(`block`);
-	div.addEventListener(`mouseenter`, () => div.classList.add(`hovered`));
-	grid.appendChild(div);
+function generateGrid (size) {
+	if (size > 100) {
+		alert(`max grid size is 100. please input a new size.`);
+		return;
+	}
+
+	const previousGrid = document.querySelectorAll(`.block`);
+	previousGrid.forEach((element) => grid.removeChild(element));
+
+	for (let i = 0; i < (size*size); i++) {
+		const div = document.createElement(`div`);
+
+		const blockDimension = (1 / size) * 100;
+		div.style.height = `${blockDimension}%`;
+		div.style.width = `${blockDimension}%`;
+
+		div.classList.add(`block`);
+
+		div.addEventListener(`mouseenter`, () => div.classList.add(`hovered`));
+
+		grid.appendChild(div);
+	}
 }
+
+const input = document.querySelector(`input`);
+const btn = document.querySelector(`button`);
+
+btn.addEventListener(`click`, () => generateGrid(input.value));
