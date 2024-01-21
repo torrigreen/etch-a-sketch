abb752b6bd23860faf59cbcee05c42d732af0a65
torrigreen <tgreen14@tulane.edu>
2024-01-21 14:38:43 -0600

    Implement hover effect

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const grid = document.querySelector(`.grid`);
 for (let i = 0; i < 256; i++) {
 	const div = document.createElement(`div`);
 	div.classList.add(`block`);
+	div.addEventListener(`mouseenter`, () => div.classList.add(`hovered`));
 	grid.appendChild(div);
 }
